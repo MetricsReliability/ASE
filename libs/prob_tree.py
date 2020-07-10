52f7a8f03c5e8db2887c71077ff4eaae2b7b1c20
nimashiri <nimashiri2012@gmail.com>
2020-07-11 01:51:31 +0430

    CITree oo-structured

--- libs/prob_tree.py
+++ libs/prob_tree.py
@@ -1,22 +1,8 @@
 import ctypes
-import itertools
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report, roc_auc_score
-from sklearn import cluster
-
-alpha = 2.220446049250313e-16
-
-
-def binerize_class(data):
-    num_records = data.shape[0]
-    for r in range(num_records):
-        if data.iloc[r, -1] > 0:
-            data.iloc[r, -1] = 2
-        else:
-            data.iloc[r, -1] = 1
-    return data
 
 
 def code(x_slow, y_slow):
@@ -108,138 +94,125 @@ class BuildStructure:
         return tree
 
 
-def make_prob(whole_data, tree_indicator):
-    num_tree = 2
-    # tree_indicator = [[0, 1, 2, 3, 4, 5], [6, 7, 8, 9, 10, 11], [12, 13, 14, 15, 16, 17, 18, 19, 20]]
-    tree_params = []
-    uc = np.unique(whole_data.iloc[:, -1])
+class ConditionalIndependenceTree:
+    def __init__(self, alpha):
+        self.alpha = alpha
 
-    for i in range(len(tree_indicator)):
-        tree_params = tree_params + [[]]
-        current_tree = tree_indicator[i]
-        # unique_holder = unique_holder + [[]]
-        # size_holder = size_holder + [[]]
+    def make_prob(self, data, tree_indicator):
+        tree_params = []
+        uc = np.unique(data.iloc[:, -1])
+
+        for i in range(len(tree_indicator)):
+            tree_params = tree_params + [[]]
+            current_tree = tree_indicator[i]
+            for c in range(len(uc)):
+                tree_params[i] = tree_params[i] + [[]]
+                for j in range(len(current_tree)):
+                    aj = current_tree[j]
+                    uai = np.unique(data.iloc[:, aj])
+                    tree_params[i][c] = tree_params[i][c] + [[]]
+                    for k in range(np.size(uai, 0)):
+                        tree_params[i][c][j] = tree_params[i][c][j] + [[]]
+
+        return tree_params, tree_indicator
+
+    def fit(self, data):
+
+        self.clust = self.identify_subtree(data)
+
+        self.tree_params, self.tree_indicator = self.make_prob(data, self.clust)
+
+        n, N = data.shape
+        uc = np.unique(data.iloc[:, -1])
+        self.pclass = np.zeros((1, len(uc)))
         for c in range(len(uc)):
-            tree_params[i] = tree_params[i] + [[]]
-            for j in range(len(current_tree)):
-                aj = current_tree[j]
-                uai = np.unique(whole_data.iloc[:, aj])
-                tree_params[i][c] = tree_params[i][c] + [[]]
-                # unique_holder[i] = unique_holder[i] + [uai]
-                # size_holder[i] = size_holder[i] + [uai]
-                for k in range(np.size(uai, 0)):
-                    tree_params[i][c][j] = tree_params[i][c][j] + [[]]
+            self.pclass[0, c] = len(data[data.iloc[:, -1] == uc[c]]) / n
 
-    return tree_params, tree_indicator
-
-
-def product(ar_list):
-    if not ar_list:
-        yield ()
-    else:
-        for a in ar_list[0]:
-            for prod in product(ar_list[1:]):
-                yield (a,) + prod
-
-
-def fit(tree_params, tree_indicator, data):
-    n, N = data.shape
-    uc = np.unique(data.iloc[:, -1])
-    pclass = np.zeros((1, len(uc)))
-    for c in range(len(uc)):
-        pclass[0, c] = len(data[data.iloc[:, -1] == uc[c]]) / n
-
-    unique_holder = []
-    size_holder = []
-    for i in range(len(tree_params)):
-        unique_holder = unique_holder + [[]]
-        size_holder = size_holder + [[]]
-        current_cluster = tree_indicator[i]
-        for j in range(len(current_cluster)):
-            aj = current_cluster[j]
-            uaj = list(np.unique(data.iloc[:, aj]))
-            unique_holder[i] = unique_holder[i] + [uaj]
-            size_holder[i] = size_holder[i] + [len(uaj)]
-        # unique_holder[i] = list(product(unique_holder[i]))
-
-    for i in range(len(tree_params)):
-        current_cluster = tree_indicator[i]
-        for c in range(len(uc)):
-            # for u in unique_holder[i]:
-            # for k, val in enumerate(u):
+        self.unique_holder = []
+        self.size_holder = []
+        for i in range(len(self.tree_params)):
+            self.unique_holder = self.unique_holder + [[]]
+            self.size_holder = self.size_holder + [[]]
+            current_cluster = self.tree_indicator[i]
             for j in range(len(current_cluster)):
                 aj = current_cluster[j]
-                # for k in range(np.size(tree_params[i][c][j], 0)):
-                for k in range(size_holder[i][j]):
-                    idx = unique_holder[i][j]
-                    tree_params[i][c][j][k] = (
-                            len(np.where((data.iloc[:, aj] == idx[k]) & (data.iloc[:, -1] == uc[c]))[0]) / len(
-                        np.where((data.iloc[:, -1] == uc[c]))[0]))
-                    if tree_params[i][c][j][k] == 0:
-                        tree_params[i][c][j][k] = 0.01
-                    tree_params[i][c][j][k] = np.log2(tree_params[i][c][j][k])
-    return tree_params, pclass, unique_holder
+                uaj = list(np.unique(data.iloc[:, aj]))
+                self.unique_holder[i] = self.unique_holder[i] + [uaj]
+                self.size_holder[i] = self.size_holder[i] + [len(uaj)]
 
+        for i in range(len(self.tree_params)):
+            current_cluster = self.tree_indicator[i]
+            for c in range(len(uc)):
+                for j in range(len(current_cluster)):
+                    aj = current_cluster[j]
+                    for k in range(self.size_holder[i][j]):
+                        idx = self.unique_holder[i][j]
+                        self.tree_params[i][c][j][k] = (
+                                len(np.where((data.iloc[:, aj] == idx[k]) & (data.iloc[:, -1] == uc[c]))[0]) / len(
+                            np.where((data.iloc[:, -1] == uc[c]))[0]))
+                        if self.tree_params[i][c][j][k] == 0:
+                            self.tree_params[i][c][j][k] = self.alpha
+                        self.tree_params[i][c][j][k] = np.log2(self.tree_params[i][c][j][k])
+        return self
 
-def predict(tree_params, pclass, tree_indicator, test_data, unique_holder):
-    output = []
-    prob = []
-    for c in range(np.size(pclass, 1)):
-        ti = 0
-        prob = prob + [[]]
-        for i in range(np.size(tree_params, 0)):
-            current_cluster = tree_indicator[i]
-            temp = 0
-            for j in range(np.size(current_cluster, 0)):
-                aj = current_cluster[j]
-                idx = test_data.iloc[aj]
-                if idx in unique_holder[i][j]:
-                    index = unique_holder[i][j].index(idx)
-                    temp += tree_params[i][c][j][index]
-                else:
-                    temp += np.log2(1 / np.size(pclass, 1))
-            ti += temp
-        # ti += pclass[0, c]
-        prob[c] = ti
-        prob[c] = 2 ** prob[c]
-    output.append(prob)
-    return np.argmax(prob)
+    def predict(self, test_data):
+        pred = []
+        for t in range(len(test_data)):
+            instance = test_data.iloc[t, :]
+            output = []
+            prob = []
+            for c in range(np.size(self.pclass, 1)):
+                ti = 0
+                prob = prob + [[]]
+                for i in range(np.size(self.tree_params, 0)):
+                    current_cluster = self.tree_indicator[i]
+                    temp = 0
+                    for j in range(np.size(current_cluster, 0)):
+                        aj = current_cluster[j]
+                        idx = instance.iloc[aj]
+                        if idx in self.unique_holder[i][j]:
+                            index = self.unique_holder[i][j].index(idx)
+                            temp += self.tree_params[i][c][j][index]
+                        else:
+                            temp += np.log2(1 / np.size(self.pclass, 1))
+                    ti += temp
+                prob[c] = ti
+                prob[c] = 2 ** prob[c]
+            output.append(prob)
+            pred.append(np.argmax(prob))
+        return pred
 
-
-def identify_subtree(data):
-    X = data.iloc[:, 0:-1].values
-    y = data.iloc[:, -1].values
-    struct = BuildStructure(X, y)
-    n, N = data.shape
-    if N <= 5:
-        n_cluster = 2
-    else:
-        n_cluster = round(N / 2)
-    sub_trees = struct.structure(n_cluster)
-    return sub_trees
+    def identify_subtree(self, data):
+        X = data.iloc[:, 0:-1].values
+        y = data.iloc[:, -1].values
+        struct = BuildStructure(X, y)
+        n, N = data.shape
+        if N - 1 <= 5:
+            n_cluster = 2
+        else:
+            n_cluster = round(np.divide(N-1, 2))
+        sub_trees = struct.structure(n_cluster)
+        return sub_trees
 
 
 def main():
     data = pd.read_csv("E:\\apply\\york\\project\\source\\datasets\\credit-g.csv")
-    #train_data = pd.read_csv("xerces-1.2.csv")
-    #test_data = pd.read_csv("xerces-1.3.csv")
+    # train_data = pd.read_csv("xerces-1.2.csv")
+    # test_data = pd.read_csv("xerces-1.3.csv")
 
-    #train_data = binerize_class(train_data)
-    #test_data = binerize_class(test_data)
+    # train_data = binerize_class(train_data)
+    # test_data = binerize_class(test_data)
 
     train_data, test_data = train_test_split(data, shuffle=True, test_size=0.33, random_state=42)
 
-    clust = identify_subtree(train_data)
+    citree = ConditionalIndependenceTree(alpha=0.01)
 
-    tree_params, tree_indicator = make_prob(train_data, clust)
-    tree_params, pclass, unique_holder = fit(tree_params, clust, train_data)
-    pred = []
-    for i in range(len(test_data)):
-        pred.append(predict(tree_params, pclass, clust, test_data.iloc[i, :], unique_holder))
+    citree.fit(train_data)
+    pred = citree.predict(test_data)
 
     print("CITree Classification Accuracy:", accuracy_score(test_data.iloc[:, -1], pred))
     print(classification_report(test_data.iloc[:, -1], pred))
-    # print("AUC:", roc_auc_score(test_data.iloc[:, -1], pred, average=None))
+    print("AUC:", roc_auc_score(test_data.iloc[:, -1], pred, average=None))
 
 
 if __name__ == '__main__':
